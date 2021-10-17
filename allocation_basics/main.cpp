082c9631902d2b9860952d091ac4e1222cf9794b
MrRoteks <rotkol@wp.pl>
2021-10-17 21:38:27 +0200

    added simpleCalc

---
+++ allocation_basics/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+
+    //int l = stoi(_Str:argv[3]);
+//        switch (argv[1]) {
+//            case "add":
+//                cout>>"Podaj liczby do dodania: "
+//                break;
+//
+//            case "substract":
+//                //jakiś kod
+//                break;
+//
+//            case "volume":
+//                //jakiś kod
+//                break;
+//
+//            case "help":
+//                //jakiś kod
+//                break;
+//
+//            default:
+//                //jakiś kod
+//                break;
+//        }
+
+
+
+    /*for(int i = 0; i < argc ; ++i)
+    {
+        cout << argv[i] <<endl;
+    }
+
+    int *n;
+    n=new int;
+    int k;
+
+   // cout<< n<<endl <<*n<<endl <<&n<<endl <<&k<<endl;
+
+    delete n;
+
+    int *tab = new int [5]; //tabela
+
+    delete []tab;
+
+    int **tab2 = new int*[5];
+
+
+    for(int i=0;i<5;i++)
+    {
+        tab2[i]=new int[5];
+    }
+
+    int tab3[5]={3,2,1,5,6};
+
+*/
+    return 0;
+}
