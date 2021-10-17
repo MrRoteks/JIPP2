a1dedc4257b47b6a04f738ca22b681bce5a91e8e
MrRoteks <rotkol@wp.pl>
2021-10-17 21:26:03 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/JIPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JIPP.dir/main.cpp.obj"
+  "JIPP.exe"
+  "JIPP.exe.manifest"
+  "JIPP.pdb"
+  "libJIPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JIPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
