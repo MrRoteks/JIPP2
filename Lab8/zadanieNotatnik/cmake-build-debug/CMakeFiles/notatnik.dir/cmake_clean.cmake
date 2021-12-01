ec15e3fc884316b10a2a66b1c50284d8a39f43e0
Lukasz Rotko <lukasz.rotko@student.pk.edu.pl>
2021-12-01 19:02:13 +0100

    added labs 6-8

---
+++ Lab8/zadanieNotatnik/cmake-build-debug/CMakeFiles/notatnik.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/notatnik.dir/src/main.cpp.obj"
+  "CMakeFiles/notatnik.dir/src/notatnik.cpp.obj"
+  "libnotatnik.dll.a"
+  "notatnik.exe"
+  "notatnik.exe.manifest"
+  "notatnik.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/notatnik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projekt1/cmake-build-debug/CMakeFiles/matrixApp.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/rotko/CLionProjects/JIPP2/Lab4/Klasy_i_Obiekty/Przyklad/klasyIobiekty.cpp" "C:/Users/rotko/CLionProjects/JIPP2/Projekt1/cmake-build-debug/CMakeFiles/matrixApp.dir/C_/Users/rotko/CLionProjects/JIPP2/Lab4/Klasy_i_Obiekty/Przyklad/klasyIobiekty.cpp.obj"
+  "C:/Users/rotko/CLionProjects/JIPP2/Projekt1/matrixApp/main.cpp" "C:/Users/rotko/CLionProjects/JIPP2/Projekt1/cmake-build-debug/CMakeFiles/matrixApp.dir/matrixApp/main.cpp.obj"
+  "C:/Users/rotko/CLionProjects/JIPP2/Projekt1/matrixLib/src/matrixLib.cpp" "C:/Users/rotko/CLionProjects/JIPP2/Projekt1/cmake-build-debug/CMakeFiles/matrixApp.dir/matrixLib/src/matrixLib.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../matrixLib/include"
+  "../matrixLib/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
